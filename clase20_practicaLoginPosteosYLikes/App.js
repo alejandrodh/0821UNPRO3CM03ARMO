e6c77d976ee21f3fdabe31552ad978860a1f6183
alejandrdh <alejandro@digitalhouse.com>
2021-10-28 11:06:16 -0300

    Repaso login avanzado, postear y likes.

---
+++ clase20_practicaLoginPosteosYLikes/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import Home from './src/screens/home';
+import Menu from './src/components/Menu';
+
+function App() {
+  return (
+    <Menu />
+  );
+}
+
+export default App;
